a096ae1cf1a9690e7b735c6b09a8f904ec545114
sadegh jafari <sadeghjafari5079@gmail.com>
2025-11-20 12:41:28 +0100

    the google api key has been deleted

--- src/config.py
+++ src/config.py
@@ -4,7 +4,6 @@ import os
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
 
 RESULT_DIR = os.getenv("RESULT_DIR", "results")
